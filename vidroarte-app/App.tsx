95d9b78fe8d3f92951cb54a98f0ef4c67e60efc2
Maria Luiza <marialuizagdesousa@gmail.com>
2023-09-16 21:16:57 -0300

    projeto criado

---
+++ vidroarte-app/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import {THEME} from './src/theme'
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <View/> : <Loading />}
+
+    </NativeBaseProvider>
+  );
+}
