a06ce59f5e39e82fc1363b73ad09ca2cc544fd87
Maria Luiza <marialuizagdesousa@gmail.com>
2023-10-14 19:51:59 -0300

    criação de rotas do aplicativo

---
+++ vidroarte-app/src/screens/OrderDetailing.js
@@ -0,0 +1,9 @@
+import { Center, Text } from 'native-base';
+
+export function OrderDetailing() {
+  return (
+    <Center flex={1}>
+      <Text color="white">OrderDetailing</Text>
+    </Center>
+  );
+}
---
+++ vidroarte-app/src/screens/Profile.js
@@ -0,0 +1,9 @@
+import { Center, Text } from 'native-base';
+
+export function Profile() {
+  return (
+    <Center flex={1}>
+      <Text color="white">Profile</Text>
+    </Center>
+  );
+}
